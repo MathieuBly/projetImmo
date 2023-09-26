9591ec80c537b34e78322641ec2c8ed3c4fbbcdc
MathieuBly <mathieubellay99@gmail.com>
2023-09-26 11:24:20 +0200

    tamplate + login

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -29,9 +29,9 @@ class DashboardController extends AbstractDashboardController
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
-        if ('jane' === $this->getUser()->getUsername()) {
-            return $this->redirect('...');
-        }
+        //if ('jane' === $this->getUser()->getUsername()) {
+        //    return $this->redirect('...');
+        //}
 
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
